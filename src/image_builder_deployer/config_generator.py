c42901fdea0b0ed374caf3836a19b8fa77fd49d7
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-12-01 10:26:37 +0000

    make basic logic

---
+++ src/image_builder_deployer/config_generator.py
@@ -0,0 +1,16 @@
+import shutil
+import configparser
+import pathlib
+
+def main() -> None:
+    config = configparser.ConfigParser()
+    path = pathlib.Path.cwd() / "config" / "config_template.ini"
+    new_path = pathlib.Path.cwd() / "config" / "config.ini"
+    config.read(path)
+    aciton_name = "test2"
+    config.set("github","action_name", aciton_name)
+    with open(new_path,"w") as f:
+        config.write(f)
+
+if __name__ == "__main__":
+    main()
